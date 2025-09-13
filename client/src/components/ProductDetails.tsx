24936ea258f43722c70bbd7328c39433da8c7d18
JPalacio3 <jpalaciocano3@gmail.com>
2025-09-13 17:48:35 -0600

    feat: implementando el botón para editar producto

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import type { Product } from "../types";
 import { formatCurrency } from "../utils";
 
@@ -7,6 +8,7 @@ type ProductDetailsProps = {
 
 export default function ProductDetails({ product }: ProductDetailsProps) {
   const isAvailable = product.availability ? "Disponible" : "No Disponible";
+  const navigate = useNavigate();
 
   return (
     <>
@@ -16,7 +18,16 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
           {formatCurrency(product.price)}
         </td>
         <td className="p-3 text-lg text-gray-800 ">{isAvailable}</td>
-        <td className="p-3 text-lg text-gray-800 "></td>
+        <td className="p-3 text-lg text-gray-800 ">
+          <div className="flex flex-col md:flex-row gap-2">
+            <button
+              onClick={() => navigate(`/productos/${product.id}/editar`)}
+              className="bg-indigo-600 text-white rounded-lg w-full uppercase font-bold text-xs text-center p-2 shadow-md hover:bg-indigo-800 "
+            >
+              Editar
+            </button>
+          </div>
+        </td>
       </tr>
     </>
   );
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Products, { loader as productsLoader } from "./views/Products";
 import NewProducts, { action as newProductsAction } from "./views/NewProducts";
+import EditProduct from "./views/EditProduct";
 
 export const router = createBrowserRouter([
   {
@@ -17,6 +18,11 @@ export const router = createBrowserRouter([
         path: "productos/nuevo",
         element: <NewProducts />,
         action: newProductsAction,
+      },
+
+      {
+        path: "productos/:id/editar",
+        element: <EditProduct />,
       },
     ],
   },
